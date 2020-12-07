fe186d003258238a82ce423db2a7527420fec1a6
Faiz Hidayatulloh <faiz14hiedayatoelloh@gmail.com>
2020-12-07 21:33:01 +0700

    customize error page

---
+++ application/views/errors/401.php
@@ -0,0 +1,1 @@
+<!-- akses ditolak -->
---
+++ application/views/errors/404.php
@@ -0,0 +1,29 @@
+<!-- halaman tidak ditemukan -->
+<div class="page-inner">
+    <h1>404</h1>
+    <div class="page-description">
+        Halaman Tidak ditemukan!!!
+    </div>
+    <div class="page-search">
+    <!-- <form>              	
+        <div class="form-group floating-addon floating-addon-not-append">
+        <div class="input-group">
+            <div class="input-group-prepend">
+            <div class="input-group-text">                          
+                <i class="fas fa-search"></i>
+            </div>
+            </div>
+            <input type="text" class="form-control" placeholder="Search">
+            <div class="input-group-append">
+            <button class="btn btn-primary btn-lg">
+                Search
+            </button>
+            </div>
+        </div>
+        </div>
+    </form> -->
+    <div class="mt-3">
+        <a href="<?=base_url('auth');?>">Kembali ke Home</a>
+    </div>
+    </div>
+</div>
